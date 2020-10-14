95e252fb3fa644f4ccea3ff83378ce0f68fd6770
Vu Van Hung <38419968+KINGKONG2808@users.noreply.github.com>
2020-10-15 02:08:09 +0700

    create database

---
+++ sql/query.sql
@@ -0,0 +1,79 @@
+/*create database for lmo_g9*/
+use master
+go
+/*create databae*/
+create database lmo_g9
+go
+/*create table*/
+-- account
+create table account (
+	account_id int identity(1, 1) not null primary key,
+	fullname nvarchar(200),
+	address nvarchar(200),
+	date_of_birth varchar(200),
+	role_id int, -- 0: user, 1: admin
+	username varchar(200),
+	password varchar(200),
+	avatar_path varchar(200),
+	create_date datetime,
+	create_by int,
+	update_date datetime,
+	update_by int
+)
+go
+-- type
+create table type (
+	type_id int identity(1, 1) not null primary key,
+	name nvarchar(200),
+	create_date datetime,
+	create_by int,
+	update_date datetime,
+	update_by int
+)
+go
+-- singer
+create table singer (
+	singer_id int identity(1, 1) not null primary key,
+	name nvarchar(200),
+	create_date datetime,
+	create_by int,
+	update_date datetime,
+	update_by int
+)
+go
+-- music
+create table music (
+	music_id int identity(1, 1) not null primary key,
+	name nvarchar(200),
+	file_path varchar(200),
+	image_path varchar(200),
+	singer_id int,
+	type_id int,
+	create_date datetime,
+	create_by int,
+	update_date datetime,
+	update_by int
+)
+go
+-- music factory
+create table music_factory (
+	music_factory_id int identity(1, 1) not null primary key,
+	music_id int,
+	singer_id int,
+	create_date datetime,
+	create_by int,
+	update_date datetime,
+	update_by int
+)
+go
+-- music_favorite
+create table music_favorite (
+	music_favorite_id int identity(1, 1) not null primary key,
+	music_id int,
+	account_id int,
+	create_date datetime,
+	create_by int,
+	update_date datetime,
+	update_by int
+)
+go
