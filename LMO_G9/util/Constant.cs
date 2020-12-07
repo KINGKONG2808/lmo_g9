33a346e4daea391f5cc913031c1d87f52531fcb3
Vu Van Hung <38419968+KINGKONG2808@users.noreply.github.com>
2020-12-07 19:42:47 +0700

    update constant

--- LMO_G9/util/Constant.cs
+++ LMO_G9/util/Constant.cs
@@ -8,6 +8,8 @@ namespace LMO_G9.util
     public class Constant
     {
         // connect string config
+        public static string CONNECTING_STRING_K2 = @"Data Source=KINGKONG;Initial Catalog=lmo_g9;Integrated Security=True";
+        public static string CONNECTING_STRING_NIET = @"Data Source=NIET\SQLEXPRESS;Initial Catalog=lmo_g9;Integrated Security=True";
         public static string CONNECTING_STRING_HIEU = @"Data Source=DESKTOP-L3U3G83;Initial Catalog=lmo_g9;Integrated Security=True";
     }
 }
