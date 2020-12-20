6636655a1ee291e753ca18288fa1905dea336782
Vu Van Hung <38419968+KINGKONG2808@users.noreply.github.com>
2020-12-21 02:17:10 +0700

    delete trash page

--- LMO_G9/view/admin/template-admin.master.cs
+++ LMO_G9/view/admin/template-admin.master.cs
@@ -11,14 +11,23 @@ namespace LMO_G9.view.admin
     public partial class template_admin : System.Web.UI.MasterPage
     {
         public Account account;
-        public string fullname;
+        public string fullname = "";
 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                account = (Account)Session["account"];
-                fullname = account.Fullname == null ? "ADMIN" : account.Fullname;
+                if (HttpContext.Current.Session["account"] != null)
+                {
+                    account = (Account)Session["account"];
+                    if (account.Fullname != null)
+                    {
+                        fullname = account.Fullname;
+                    }
+                } else
+                {
+                    Response.Redirect("~/view/library/404.html");
+                }
             }
         }
     }
